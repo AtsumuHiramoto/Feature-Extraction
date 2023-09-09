2d4a71ce88966d5d1e2f7df87f5f21ebf7bf30ca
A.Hiramoto <hotdream37@gmail.com>
2023-09-09 15:38:34 +0900

    Connect make_dataset.py

--- train_ccae.py
+++ train_ccae.py
@@ -10,6 +10,7 @@ from torch.utils.data import Dataset
 from torchsummary import summary
 from argparse import ArgumentParser
 import yaml
+from make_dataset import MyDataset
 
 def get_option():
     argparser = ArgumentParser()
@@ -77,28 +78,6 @@ def main():
     # saveimg=True
     # if saveimg:
     #     saveCoordinatesImage(handlingData,tactileCoordinatesName)
-
-class MyDataset(Dataset):
-    def __init__(self, data, inputType, device, transform=None) -> None:
-        super().__init__()
-        self.data = data
-        self.inputType = inputType
-        self.device = device
-        self.csv_num = len(self.data)
-        self.timestep = self.data[0][self.inputType[0]].shape[0]
-
-    def __getitem__(self, index):
-        data_dict = dict()
-        for key in self.inputType:
-            data_dict[key] = torch.tensor(self.data[int(index/self.timestep)][key][index%self.timestep,:]).to(self.device)
-            # data_dict[key] = torch.tensor(self.data[int(index/self.timestep)][key][index%self.timestep,:])
-            # data_dict[key] = data_dict[key].clone().detach().to(self.device)
-        return data_dict
-        # return data_dict, int(index/self.timestep), index%self.timestep
-
-    def __len__(self) -> int:
-        self.length = self.csv_num * self.timestep
-        return self.length
 
 if __name__=="__main__":
     main()
