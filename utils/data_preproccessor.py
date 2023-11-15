fix normalization

utils/data_preproccessor.py
@@ -248,7 +248,8 @@ class DataPreprocessor(object):
                                  scaling_range="patch", 
                                  separate_axis=True, 
                                  separate_joint=True,
-                                 tactile_scale=None):
+                                 tactile_scale=None,
+                                 normalization_range=[0.0,1.0]):
         """
         Function to scale data.
         The scaling parameters are saved in self.scaling_df
@@ -277,6 +278,7 @@ class DataPreprocessor(object):
 
         self.input_data = input_data
         self.output_data = output_data
+        self.normalization_range = normalization_range
 
         # self.scaling_param = {"scaling_mode" : scaling_mode, 
         #                       "scaling_range" : scaling_range, 
@@ -322,14 +324,9 @@ class DataPreprocessor(object):
             if tactile_scale=="sqrt":
                 self.handling_data["data"][:, tactile_column] = self.handling_data["data"][:, tactile_column] / torch.sqrt(torch.abs(self.handling_data["data"][:, tactile_column]) + 10e-5)
             elif tactile_scale=="log":
-<<<<<<< HEAD
-                self.handling_data["data"][:, tactile_column] = torch.log(self.handling_data["data"][:, tactile_column] + 1.0) * self.handling_data["data"][:, tactile_column] / torch.abs(self.handling_data["data"][:, tactile_column] + 10e-5)
-        # import ipdb; ipdb.set_trace()
-=======
                 # import ipdb; ipdb.set_trace()
                 self.handling_data["data"][:, tactile_column] = torch.log(torch.abs(self.handling_data["data"][:, tactile_column]) + 1.0) * self.handling_data["data"][:, tactile_column] / torch.abs(self.handling_data["data"][:, tactile_column] + 10e-5)
             # import ipdb; ipdb.set_trace()
->>>>>>> 0ec1a25f3519e946616ca29a7ebdc67aa4df100e
 
         if scaling_range=="patch":
             if separate_axis==True:
@@ -457,7 +454,12 @@ class DataPreprocessor(object):
         # Save scaling parameters
         self.scaling_df.loc["max"][target_column] = df_max
         self.scaling_df.loc["min"][target_column] = df_min
-        self.handling_data["data"][:, target_column] = (self.handling_data["data"][:, target_column] - df_min) / (df_max - df_min)
+
+        data_min = self.normalization_range[0]
+        data_max = self.normalization_range[1]
+        data_range = data_max - data_min
+        # self.handling_data["data"][:, target_column] = (self.handling_data["data"][:, target_column] - df_min) / (df_max - df_min)
+        self.handling_data["data"][:, target_column] = (((self.handling_data["data"][:, target_column] - df_min) * data_range) / (df_max - df_min)) + data_min
 
     def standardization(self, target_column):
         """

utils/make_dataset.py
@@ -15,19 +15,31 @@ class MyDataset(Dataset):
             self.data_length = handling_data["test_data_length"]
         self.dataset_num = len(data)
         columns = handling_data["columns"]
-        joint_mask = [bool(re.match("Joint", s)) for s in columns]
-        self.joint_data = data[:,:,joint_mask].float()
-        desjoint_mask = [bool(re.match("DesJoint", s)) for s in columns]
-        self.desjoint_data = data[:,:,desjoint_mask].float()
-        tactile_mask = [bool(re.match(".*Tactile", s)) for s in columns]
-        self.tactile_data = data[:,:,tactile_mask].float()
-        torque_mask = [bool(re.match("Torque", s)) for s in columns]
-        self.torque_data = data[:,:,torque_mask].float()
+        if "joint" in input_data:
+            joint_mask = [bool(re.match("Joint", s)) for s in columns]
+            self.joint_data = data[:,:,joint_mask].float()
+        if "desjoint" in output_data:
+            desjoint_mask = [bool(re.match("DesJoint", s)) for s in columns]
+            self.desjoint_data = data[:,:,desjoint_mask].float()
+        if "tactile" in input_data:
+            tactile_mask = [bool(re.match(".*Tactile", s)) for s in columns]
+            self.tactile_data = data[:,:,tactile_mask].float()
+        if "torque" in input_data:
+            torque_mask = [bool(re.match("Torque", s)) for s in columns]
+            self.torque_data = data[:,:,torque_mask].float()
         if "label" in output_data:
             label_mask = [bool(re.match("Label", s)) for s in columns]
             self.label = data[:,:,label_mask].int()
-        self.file_names = handling_data["load_files"]
+        if "thumb" in input_data:
+            # rewrite each modalities
+            joint_mask = [bool(re.match("JointF3", s)) for s in columns]
+            self.joint_data = data[:,:,joint_mask].float()
+            tactile_mask = [bool(re.match("Thumb.*Tactile", s)) for s in columns]
+            self.tactile_data = data[:,:,tactile_mask].float()
+            torque_mask = [bool(re.match("TorqueF3", s)) for s in columns]
+            self.torque_data = data[:,:,torque_mask].float()
         # import ipdb; ipdb.set_trace()
+        self.file_names = handling_data["load_files"]
         self.input_data = input_data
         self.output_data = output_data
         # self.device = device
@@ -42,7 +54,6 @@ class MyDataset(Dataset):
         if "joint" in self.input_data:
             # joint_data = torch.t(joint_data).float()
             # add gaussian noise to input data
-            # import ipdb; ipdb.set_trace()
             if self.stdev_joint > 0:
                 x_data["joint"] = self.joint_data[index] + torch.normal(mean=0, std=self.stdev_joint, size=self.joint_data[index].shape)
             else:
@@ -69,9 +80,9 @@ class MyDataset(Dataset):
                 x_data["torque"] = self.torque_data[index]
         if "torque" in self.output_data:
             y_data["torque"] = self.torque_data[index]
-        data_length = self.data_length[index]
         if "label" in self.output_data:
             y_data["label"] = self.label[index]
+        data_length = self.data_length[index]
         file_name = self.file_names[index]
         return [x_data, y_data, data_length, file_name]
 